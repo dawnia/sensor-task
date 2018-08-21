b292ece2fb25e642e7962435569e081e93461bd6
Dawnia Brody <dkbrody@berkeley.edu>
2018-08-21 12:39:32 -0700

    Obj Oriented

--- main.py
+++ main.py
@@ -9,49 +9,81 @@ Future protocol:
 5. Run game update() at sampling rate (50 Hz)
 6. Game reads from sensors and determines game state
 7. Cycles through state transformation dictionary on state of each trial
-8. Write a) time, b) sensor values, c) trial number, d) trial type, e) trial state at every task loop
+8. Write a) time, b) sensor values, c) trial number, d) trial type, e) trial state at every task loop, start sound, end sound
 
 Considerations
-- How to save data the entire time, while managing the task? --> threading
+- How to save data the entire time, while managing the task? --> threading? a lot of calls? Two programs, match up times? Need to maintain 50 Hz
 - need to write to Arduino?
 - timing of conditions depends on task design
-- Functions :D
+- functions
+- Table
 """
 
 import serial
 import csv
 import time
-from datetime import datetime
-from playsound import playsound
+import pygame
 
-# Connect to Arduino
-port = serial.Serial('/dev/tty.usbmodem1411', 9600)
-time.sleep(2)   # 2 sec to establish connection with Arduino
+class Experiment():
 
-# Number of trials and trial length
-numTrials = int(input('Enter number of trials to start\n'))
-trial_len = 4
+	def __init__(self, trial_len=5, my_port='/dev/tty.usbmodem1411'):
+		self.trial_len = trial_len
+		# self.trial_amt = int(input('Enter number of trials to start\n'))
+		self.sound = False
+		self.cur_state = 'init'
+		self.my_port = my_port
+		self.my_serial = self.connect_arduino()
+		pygame.mixer.init()
+		self.run_trials()
 
-for trial in range(numTrials):
+	def run_trials(self):
+		# for trial_num in range(self.trial_amt):
+		trial_num = 1
+		start_time = time.time()
+		while time.time() < start_time + 600:    # 10 min max
+			next_task = str(input('Enter type number of next task or DONE to quit.\n'))
+			if next_task == 'DONE':
+				break
+			else:
+				self.cur_state = 'task_' + next_task
+			print('Trial starting...')
+			self.sound = pygame.mixer.get_busy()
+			self.play_sound('start')
+			time_end = time.time() + self.trial_len
+			while time.time() < time_end:
+				data = self.read_arduino()
+				self.save_data(data, trial_num)
+			self.play_sound('end')
+			self.cur_state = 'btwn_task'
+			print('Trial ended.')
+			trial_num += 1
+		self.my_serial.close()
 
-	time_end = time.time() + trial_len
-	playsound('start_beep.mp3')
-	
-	while time.time() < time_end:   # the next 10 sec
-	
-		# read sensor value from Arduino
-	    data = port.readline()
-	    data = data.decode("utf-8")    # bytes to string
-	    data = int(''.join(filter(str.isdigit, data)))
-	    print(data)	  # extract digits
+	def connect_arduino(self):
+		my_serial = serial.Serial(self.my_port, 9600)
+		print('Connecting Arduino.')
+		time.sleep(2)    # 2 sec to establish connection with Arduino
+		return my_serial
 
-	    # save to csv
-	    with open('test.csv', 'a') as f:
-	    	writer = csv.writer(f, delimiter=',')
-	    	curTime = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')
-	    	writer.writerow([curTime, data, 'trial' + str(trial)])
+	def read_arduino(self):
+		''' Read sensor value from Arduino. '''
+		data = self.my_serial.readline()
+		data = data.decode('utf-8')    # bytes to string
+		data = int(''.join(filter(str.isdigit, data)))    # extract digits
+		return data
 
-	playsound('end_beep.mp3')
-	time.sleep(1)
+	def save_data(self, data, trial_num):
+		''' Save data to csv. '''
+		with open('test.csv', 'a') as f:
+			writer = csv.writer(f, delimiter=',')
+			writer.writerow([time.time(), data, 'trial' + str(trial_num), self.cur_state, self.sound])
 
-port.close()
+	def play_sound(self, state):
+		if state == 'start':
+			pygame.mixer.music.load('start_beep.mp3')
+			pygame.mixer.music.play()
+		elif state == 'end':
+			pygame.mixer.music.load('end_beep.mp3')
+			pygame.mixer.music.play()
+
+Experiment()
